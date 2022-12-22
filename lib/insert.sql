373e1d7b6cd9627788b203a4c7c10d31a22bc678
Nick Story <storynickolasl@gmail.com>
2022-12-21 20:51:06 -0500

    commit tree d56926893d983182962a04e63d92db4293bc0ec3

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive	) VALUES ("Mr. Chocolate", 12, "M", "brown", "sleepy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive	) VALUES ("Rowdy", 8, "F", "white", "rowdy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive	) VALUES ("Tabitha", 5, "F", "brown", "feisty", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive	) VALUES ("Sergeant Brown", 8, "M", "brown", "grumpy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive	) VALUES ("Melissa", 12, "F", "brown", "sleepy", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive	) VALUES ("Grinch", 4, "M", "green", "feisty", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive	) VALUES ("Wendy", 12, "F", "brown", "sleepy", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive	) VALUES (NULL, 12, "M", "brown", "sleepy", true);
